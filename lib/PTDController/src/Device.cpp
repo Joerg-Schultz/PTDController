4a668a5b905e307614c9d5265ff3c5e0b7f5c9a5
Joerg-Schultz <joergsky@online.de>
2023-03-25 17:31:03 +0100

    using inline strin2mac

--- lib/PTDController/src/Device.cpp
+++ lib/PTDController/src/Device.cpp
@@ -19,7 +19,6 @@ static const char* TAG = "Device";
 
 static std::vector<esp_now_peer_info_t> controllerList = {};
 
-
 static void processSendQueue(void* parameters) {
     while (1) {
         if (uxQueueMessagesWaiting(sendQueue) > 0) {
@@ -118,13 +117,7 @@ bool startDevice(PTDdevice* device) {
             esp_now_peer_info_t controller = {};
             controller.channel = CHANNEL;
             controller.encrypt = ENCRYPT;
-            int mac[6];
-            if (6 == sscanf(BSSIDstr.c_str(), "%x:%x:%x:%x:%x:%x%c", &mac[0], &mac[1], &mac[2], &mac[3], &mac[4],
-                            &mac[5])) {
-                for (int ii = 0; ii < 6; ++ii) {
-                    controller.peer_addr[ii] = (uint8_t) mac[ii];
-                }
-            }
+            string2Mac(BSSIDstr, controller.peer_addr);
             esp_err_t addStatus = esp_now_add_peer(&controller);
             if (addStatus == ESP_OK) {
                 ESP_LOGI(TAG, "Pair success");
